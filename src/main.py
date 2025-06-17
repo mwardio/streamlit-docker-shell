9638859d2404dc8f14c87868bcfe5fbd46139479
GitHub Action <action@github.com>
2025-06-17 18:43:25 +0000

    linting action

--- src/main.py
+++ src/main.py
@@ -51,7 +51,7 @@ bar = st.progress(0)
 
 for i in range(100):
     # Update the progress bar with each iteration.
-    latest_iteration.text(f"Iteration {i+1}")
+    latest_iteration.text(f"Iteration {i + 1}")
     bar.progress(i + 1)
     time.sleep(0.05)
 
